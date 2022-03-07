3a86d383de3db99d7e05545a82663e7bdd9a3ddf
utgwkk <utagawakiki@gmail.com>
2022-03-07 20:40:56 +0900

    フトスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ShirokoImg from "./assets/shiroko.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={ShirokoImg} alt="" width={858} height={1124} />
     </div>
   );
 }
